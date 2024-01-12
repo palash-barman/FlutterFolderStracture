c04fd517721a583647f8e5829b0d7c6f7c9fb139
Palash Barman <palash900202@gmail.com>
2024-01-13 01:04:58 +0600

    update stracture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce/helper/di.dart' as di;
+import 'package:e_commerce/helpers/di.dart' as di;
 import 'package:e_commerce/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+ static const Color colorNero = Color(0xFF1F1F1F);
+  static const Color searchBg = Color(0xFFF4F7FC);
+  static const Color borderColor = Color(0xFFDCDCDC);
+  static const Color footerCol0r = Color(0xFFFFDDD9);
+  static const Color cardShadowColor = Color(0xFFA7A7A7);
+}
